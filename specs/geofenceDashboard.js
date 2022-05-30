0ba3bba8d6091ddc915bc67985b555f111a795a2
AkashMungare4420 <akashmungare@gmail.com>
2022-05-30 11:44:42 +0530

    GeofenceDashboad close button

--- specs/geofenceDashboard.js
+++ specs/geofenceDashboard.js
@@ -1,13 +1,13 @@
 const { Browser } = require("selenium-webdriver");
 const util = require('./util');
 
-function GeofenceDashboard() {
+function geofenceDashboard() {
     describe('Geofence Dashboard', () =>{
         it('Geofence Dashboard', async () =>{
             const geofenceData = $("//html/body/app-root/div/mat-sidenav-container/mat-sidenav/div/mat-list/mat-list-item[3]/div");
             const searchGD = $('//*[@data-placeholder="Search"]');
             const selectGD = $('/html/body/app-root/div/mat-sidenav-container/mat-sidenav-content/div/app-geofence-dashboard/div/div[2]/table/tbody/tr[1]/td[3]')
-            const cancelGD = $('/html/body/div[2]/div[2]/div/mat-dialog-container/app-grouping/button');
+            const cancelGD = $('//html/body/div[2]/div[2]/div/mat-dialog-container/app-grouping/button/span[1]/mat-icon');
 
             await (await geofenceData).click();
             await util.wait(2000);
@@ -21,4 +21,4 @@ function GeofenceDashboard() {
     })
 }
 
-module.exports=GeofenceDashboard();
+module.exports=geofenceDashboard();
